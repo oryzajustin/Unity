956144a39e30ec67a945e7775c1ae9ea120b54e6
Justin Koh <justinkoh@rogers.com>
2016-12-24 16:17:43 -0500

    updated player script
    this version is the same as the first upload, but added comments.

--- Player.cs
+++ Player.cs
@@ -4,18 +4,24 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
+    // variable for 2d physics on character
     private Rigidbody2D myRigidBody;
 
+    // variable for linking animation from idle to run
     private Animator myAnimator;
 
+    // allows for float to be changed in unity toolbar
     [SerializeField]
+    //variable for speed of movement
     private float speed;
 
-    private bool facingRight;
+    //variable for checking if the character is facing in the right direction
+    private bool facingRight; 
 
 	// Use this for initialization
 	private void Start ()
     {
+        //initialize at the start of the game
         facingRight = true;
         myRigidBody = GetComponent<Rigidbody2D>();
         myAnimator = GetComponent<Animator>();
@@ -24,21 +30,31 @@ public class Player : MonoBehaviour {
 	// Update is called once per frame
 	private void Update ()
     {
+        //this line of code gets the Horizontal variable in the unity toolbar's value
         float horizontal = Input.GetAxis("Horizontal");
+        //calling functions
         HandleMovement(horizontal);
         Flip(horizontal);
 	}
 
+    //handles player movement
+    //takes horizontal as parameter
     private void HandleMovement(float horizontal)
     {
+        //sets the velocity forced on the player's horizontal to the horizontal multiplied by the
+        //specified speed in the unity toolbar
         myRigidBody.velocity = new Vector2(horizontal * speed, myRigidBody.velocity.y);
+        //sets the "runspeed" trigger to the absolute value of the horizontal
         myAnimator.SetFloat("runspeed", Mathf.Abs(horizontal));
     }
 
+    //function for flipping the character to facing left/right
     private void Flip(float horizontal)
     {
+        //if you are facing the opposite way as the movement
         if(horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
         {
+            //flip the character
             facingRight = !facingRight;
             Vector3 theScale = transform.localScale;
             theScale.x *= -1;
