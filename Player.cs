4c3e598a2c3d00a9b0575b86b1dec9b1e9549f74
Justin Koh <justinkoh@rogers.com>
2016-12-24 16:08:08 -0500

    Added script for knight adventure
    this version of knight adventure has code for the player's movement, switching direction and running

---
+++ Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    private Rigidbody2D myRigidBody;
+
+    private Animator myAnimator;
+
+    [SerializeField]
+    private float speed;
+
+    private bool facingRight;
+
+	// Use this for initialization
+	private void Start ()
+    {
+        facingRight = true;
+        myRigidBody = GetComponent<Rigidbody2D>();
+        myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	private void Update ()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        HandleMovement(horizontal);
+        Flip(horizontal);
+	}
+
+    private void HandleMovement(float horizontal)
+    {
+        myRigidBody.velocity = new Vector2(horizontal * speed, myRigidBody.velocity.y);
+        myAnimator.SetFloat("runspeed", Mathf.Abs(horizontal));
+    }
+
+    private void Flip(float horizontal)
+    {
+        if(horizontal > 0 && !facingRight || horizontal < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
+    }
+}
