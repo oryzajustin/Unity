63c12d892479b1d4979be399abb0b51c2360b5a2
Justin Koh <justinkoh@rogers.com>
2016-12-27 02:33:21 -0500

    Platform script 27/12/2016 2:30 AM
    created a script for allowing the player to pass through platforms and stand on top of them

---
+++ Platform.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour {
+
+    //variable for referencing the player collider box
+    private BoxCollider2D playerCollider;
+
+    //allows for changing in unity toolbar
+    [SerializeField]
+    //variable for referencing the platform collider box
+    private BoxCollider2D platformCollider;
+
+    //allows for changing in unity toolbar
+    [SerializeField]
+    //variable for referencing the trigger collider of the platform
+    private BoxCollider2D platformTrigger;
+
+	// Use this for initialization
+	void Start ()
+    {
+        //create reference to the player box collider
+        playerCollider = GameObject.Find("Player").GetComponent<BoxCollider2D>();
+        Physics2D.IgnoreCollision(platformCollider, platformTrigger, true);
+	}
+	
+	//function for allowing player to pass through the platform
+	void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            Physics2D.IgnoreCollision(platformCollider, playerCollider, true);
+        }
+    }
+    
+    //function to set trigger again, making the player stand on the platform
+    //after passing through it
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if(other.gameObject.name == "Player")
+        {
+            Physics2D.IgnoreCollision(platformCollider, playerCollider, false);
+        }
+    }
+}
